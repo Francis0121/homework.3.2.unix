ef30a1318d95cb3dd29d2844787cacf9320e1a6d
Francis0121 <tjdrms0121@gmail.com>
2014-10-10 20:06:13 +0900

    Command Line Main 소스 분리

--- command/main.c
+++ command/main.c
@@ -8,38 +8,6 @@
 #define MAX_CHAR_SIZE 100
 #define MAX_ARGUMENT_SIZE 10
 
-/*
-*   Split Command Line Sentence
-*   @param *str
-*       type char* - command line
-*   @param *argc
-*       type int* - return argument size
-*   @return
-*       type char** - return split ' ' command line string
-*/
-char** split_command_line(char *str, int *argc){
-    char *ptr;
-    char **argv;
-    // ~ initial argument size and argument string
-    *argc = 0;
-    argv = (char **) malloc(sizeof(char*)* MAX_ARGUMENT_SIZE);
-
-    // ~ split command line and copy new array
-    ptr = strtok(str, " ");
-    *(argv+*argc) = (char *) malloc(sizeof(char) * (strlen(ptr)+1));
-    memcpy(*(argv+*argc), ptr, (strlen(ptr)+1));
-    (*argc)++;
-
-    while(ptr = strtok( NULL, " ")) {
-        // ~ split command line and copy new array
-        *(argv+*argc) = (char *) malloc(sizeof(char) * (strlen(ptr)+1));
-        memcpy(*(argv+*argc), ptr, (strlen(ptr)+1));
-        (*argc)++;
-    }
-
-    return argv;
-}
-
 /**
 *   Main
 */
@@ -54,7 +22,8 @@ int main(void) {
 
     // ~ one line read
     while(fgets(str, MAX_CHAR_SIZE, in) != NULL){
-        argv = split_command_line(str, &argc);
+        argv = (char **) malloc(sizeof(char*)* MAX_ARGUMENT_SIZE);
+        split_command_line(str, &argc, argv);
         for(i=0; i<argc; i++) {
             if(strlen(*(argv+i)) != 0){
                 printf("%s", *(argv+i));
---
+++ command/util/util.c
@@ -0,0 +1,30 @@
+#include <string.h>
+
+/*
+*   Split Command Line Sentence
+*   @param *str
+*       type char* - command line
+*   @param *argc
+*       type int* - return argument size
+*   @param **argv
+*       typ char** - return split ' ' command line string
+*/
+void split_command_line(char *str, int *argc, char **argv){
+    char *ptr;
+    // ~ initial argument size and argument string
+    *argc = 0;
+
+    // ~ split command line and copy new array
+    ptr = strtok(str, " ");
+    *(argv+*argc) = (char *) malloc(sizeof(char) * (strlen(ptr)+1));
+    memcpy(*(argv+*argc), ptr, (strlen(ptr)+1));
+    (*argc)++;
+
+    while(ptr = strtok( NULL, " ")) {
+        // ~ split command line and copy new array
+        *(argv+*argc) = (char *) malloc(sizeof(char) * (strlen(ptr)+1));
+        memcpy(*(argv+*argc), ptr, (strlen(ptr)+1));
+        (*argc)++;
+    }
+
+}
