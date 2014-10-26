cfd87462c8550b8218ce2b4d2db7e816b269b8fb
Francis0121 <tjdrms0121@gmail.com>
2014-10-26 11:25:56 +0900

    File > 완료 Background 실행도 확인

--- sh/module/file.c
+++ sh/module/file.c
@@ -47,34 +47,46 @@ int fileinput(char **cline, int *index, int where){
 * 	type int : isComplete
 */
 int fileoutput(char **cline, int *index, int where){
-	int j, fd, size;
-	char **arg;
+	pid_t pid;
+	int si, fd, str_len, status, row_size;
+	char **arg, *name;
 
-	switch (fork()) {
+	switch ( (pid =fork()) ) {
 		case -1 :
 			perror("Process fork error\n");
 		case 0 :
-			if( (fd = open(*(cline+strlen(*cline)-1), O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMS)) < 0)
-				perror("file open error\n");
+			name = *(cline + index[1]);
+			si = 0;
+			row_size = index[1];
+			arg = (char **) malloc(sizeof(char *) * row_size);
+			for( ; si < row_size; si++){
+				str_len = strlen(*(cline+si)) + 1;
+				*(arg + si) = (char *) malloc(sizeof(char) * (str_len + 1 ) );
+				memcpy( (arg + si), (cline + si), str_len);
+			}
+			*(arg+si) = NULL;
 
+			if( (fd = open(name, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMS)) < 0)
+				perror("file open error\n");
 			if (close(1) == -1)
 				perror("Process close error\n");
 			if (dup(fd) != 1)
 				perror("Process dup error\n");
-
-			size = index[1];
-			arg = (char **) malloc(sizeof(char *) * size);
-			for(j=0; j<size; j++){
-				*(arg+j) = (char *) malloc(sizeof(char) * (strlen( *(cline+j) )+2) );
-				memcpy( (arg+j), (cline+j), strlen(*(cline+j))+1 );
-			}
-			*(arg+j) = (char *) malloc(sizeof(char) * 1);
-			*(arg+j) = NULL;
 
 			execvp(*arg, arg);
 			perror(*arg);
 			exit(0);
 	}
 
-	return 0;
+	// ~ parent code
+	// ~ If it is background process this print pid and exit
+	if(where == BACKGROUND){
+		printf("[Prcess id %d]\n", pid);
+		return (0);
+	}
+
+	if(waitpid(pid, &status, 0) == -1)
+		return (-1);
+	else
+		return (status);
 }
