d36a25db0f2c577e760981fec5a81df5097e07e3
pi <tjdrms0121@gmail.com>
2014-11-04 12:36:37 +0900

    last

--- command/chmod/chmod.c
+++ command/chmod/chmod.c
@@ -3,9 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <utime.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <errno.h>
 
 #define TRUE 1
@@ -20,11 +17,11 @@ int flagRecursive = FALSE;
 * 	type *char : option string character - 'l' 'a' 'A' 'r' 'R'
 */
 void set_option_flag(char *ch){
-	switch(*ch){
-		case 'R':
-			flagRecursive = TRUE;
-			break;
-	}
+    switch(*ch){
+        case 'R':
+            flagRecursive = TRUE;
+            break;
+    }
 }
 
 /**
@@ -37,55 +34,75 @@ void set_option_flag(char *ch){
 * @param pathList
 * 	type *char : read file name or path
 */
-void control_argument(int *argc, char **argv, int *pathSize, char **pathList){
-	int i, j, strSize = 0;
-	*pathSize = 0;
-	for(i=1; i<*argc; i++){
-		if(*(*(argv+i)+0) == '\0') continue;
-		strSize = strlen(*(argv+i));
+void control_argument(int *argc, char **argv,
+        int *pathSize, char **pathList,
+        int *modeSize, char **modeList){
+    int i, j, strSize = 0;
+    char *name;
+    *pathSize = 0;
+    for(i=1; i<*argc; i++){
+        if(*(*(argv+i)+0) == '\0') continue;
+        strSize = strlen(*(argv+i));
 
-		if(*(*(argv+i)+0) == '-'){
-			for(j=1; j<strSize; j++){
-				set_option_flag(*(argv+i)+j);
-			}
-		}else{
-			*(pathList+*pathSize) = (char *) malloc(sizeof(char) * (strSize+1));
-			memcpy(*(pathList+((*pathSize)++)), *(argv+i), strSize+1);
-		}
-	}
-
+        if(*(*(argv+i)+0) == '-'){
+            for(j=1; j<strSize; j++){
+                set_option_flag(*(argv+i)+j);
+            }
+        } else if( isdigit( *(*(argv+i)+0)) ){
+            *(modeList + *modeSize) = (char *) malloc(sizeof(char) * (strSize+1));
+            memcpy(*(modeList+((*modeSize)++)), *(argv+i), strSize+1);
+            name = *(modeList+((*modeSize)-1));
+            // ~ newLine removal
+            if(name[strlen(name)-1] == '\n'){
+                name[strlen(name)-1] = '\0';
+            }
+        } else{
+            *(pathList+*pathSize) = (char *) malloc(sizeof(char) * (strSize+1));
+            memcpy(*(pathList+((*pathSize)++)), *(argv+i), strSize+1);
+            name = *(pathList+((*pathSize)-1));
+            // ~ newLine removal
+            if(name[strlen(name)-1] == '\n'){
+                name[strlen(name)-1] = '\0';
+            }
+        }
+    }
 }
 
 /**
 * @param argc
 *	type int : command line argument size at lest 2
 * @param argv
-* 	type **char : Using "cat [option] ... [file]" - command line string
+* 	type **char : Using "chmod[OPTION] … MODE[, MODE] … FILE …" - command line string
 */
 int main(int argc, char **argv) {
-	int i;
-	char *name;
+    // ~ string control variable
+    int i, mode;
+    int pathSize = 0, modeSize = 0;
+    char **pathList, **modeList;
+    char *name, *ptr;
 
-	int pathSize = 0;
-	char **pathList;
+    // ~ Argument 가 3개 이하면 종료 chmod mode file_path 필요
+    if (argc < 3) {
+        perror("chmod[OPTION] ... MODE[, MODE] ... FILE ...");
+        exit(-1);
+    }
 
-	if (argc < 3) {
-		perror("chmod[OPTION] ... MODE[, MODE] ... FILE ...\n");
-		exit(-1);
-	}
+    // ~ control to command line argument
+    pathList = (char **) malloc(sizeof(char *) * MAX_ARGUMENT_SIZE);
+    modeList = (char **) malloc(sizeof(char *) * MAX_ARGUMENT_SIZE);
+    control_argument(&argc, argv, &pathSize, pathList, &modeSize, modeList);
 
-	// ~ control to command line argument
-	pathList = (char **) malloc(sizeof(char *) * MAX_ARGUMENT_SIZE);
-	control_argument(&argc, argv, &pathSize, pathList);
+    for(i=0; i<pathSize; i++){
+        name = *(pathList + i);
+        mode = strtol(*(modeList+0), &ptr, 8); // Octat
 
-	for(i=0; i<pathSize; i++){
-		name = *(pathList+i);
-		// ~ newLine removal
-		if(name[strlen(name)-1] == '\n'){
-			name[strlen(name)-1] = '\0';
-		}
+        if( chmod(name, mode) < 0){
+            perror("chmod : system call error");
+        }
+    }
 
-	}
-
-	return 0;
+    return 0;
 }
+
+
+
