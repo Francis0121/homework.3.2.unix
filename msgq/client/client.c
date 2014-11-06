fc8726011ef7196d1c6cba0ab6caa969368cced4
pi <tjdrms0121@gmail.com>
2014-11-06 16:43:55 +0900

    mseg commit '

--- msgq/client/client.c
+++ msgq/client/client.c
@@ -1,8 +1,59 @@
 #include "../msgq.h"
+#include "../mesg.h"
 
-int main(void){
+Mesg mesg;
 
-    printf("client ...\n");
+int client(int id);
 
-    return 0;
+/**
+* Client Main
+*/
+void main(void){
+
+    int id;
+
+    // ~ Open the single message queue. The server must have already created it.
+    if ( (id = msgget(MKEY1, 0)) < 0)
+        perror("client: can't msgget message queue 1");
+
+    client(id);
+
+    // ~ Now we can delete the message queue.
+    if (msgctl(id, IPC_RMID, (struct msqid_ds *) 0) < 0)
+        perror("client: can't RMID message queue 1");
+
+    exit(0);
+
+}
+
+int client(int id){
+    int n;
+
+    // ~ Read the filename from standard input, write it as a message to the IPC descriptor.
+    if (fgets(mesg.mesg_data, MAXMESGDATA, stdin) == NULL)
+        perror("filename read error");
+
+    n = strlen(mesg.mesg_data);
+
+    // ~ ignore the newline from fgets()
+    if (mesg.mesg_data[n-1] == '\n')
+        n--;
+
+    // ~ overwrite newline at end
+    mesg.mesg_data[n] = '\0';
+    mesg.mesg_len = n;
+    // ~ send messages of this type
+    mesg.mesg_type = 1;
+    mesg_send(id, &mesg);
+
+    // ~ Receive the message from the IPC descriptor and write the data to the standard output.
+    // ~ receive messages of this type
+    mesg.mesg_type = 2;
+
+    while( (n = mesg_recv(id, &mesg)) > 0)
+        if (write(1, mesg.mesg_data, n) != n)
+            perror("data write error");
+
+    if (n < 0)
+        perror("data read error");
 }
--- msgq/msgq.h
+++ msgq/msgq.h
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/errno.h>
+
+extern int errno;
+
+#define MKEY1 1234L
+
+#define PERMS 0666
